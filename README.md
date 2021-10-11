c8c49913f5916809bd4030f165f9560457889fa1
RotateMe <RotateMe@gmail.com>
2021-10-11 15:11:59 +0200

    Added hover info

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # csv-translation-helper README
 
-The use-case for this extension is a project where you have translatable strings, which appear in the code files of your workspace exactly `"!LIKE_THIS"` (*with* the parenthesis). 
+## Overview
+
+The use-case for this extension is a project where you have translatable strings, which appear in the code files of your workspace exactly `"!LIKE_THIS"` (*with* the parenthesis). It searches for such strings across all files and adds untranslated ones to a CSV file containing the translation. Moreover, it provides hover-info on translated strings in such CSV files, namely in which files and lines the translated string is used. 
+
+# Usage
 
 This extension adds the command the command "Scan and add missing translatable strings" to the command palette, which should be run, when the translation file is open in the editor. The translation file  is assumed to be a .csv file, listing all translatable strings and their translations: 
 
@@ -9,3 +13,5 @@ This extension adds the command the command "Scan and add missing translatable s
 ```
 
 Running this command with an active file opened in the editor will search for all translatable strings in files in your workspace (except for those in the current file) and will add all translatable strings which are not yet listed in the current file to the end of the file.
+
+After having run this command, you will get hover-info when hovering over a line in a CSV file, namely all occurrences of the translated string in your code files.
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,43 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { match } from 'assert';
 import * as vscode from 'vscode';
+
+const TRANSLATABLE_REGEX = /"(![A-Z0-9_]*)"/g;
+const TRANSLATED_REGEX = /(![A-Z0-9_]*)/g;
+
+const DIALECT_MAP: { [key: string]: string[] } = {
+	'csv': [',', 'quoted'],
+	'tsv': ['\t', 'simple'],
+	'csv (semicolon)': [';', 'quoted'],
+	'csv (pipe)': ['|', 'simple'],
+	'csv (tilde)': ['~', 'simple'],
+	'csv (caret)': ['^', 'simple'],
+	'csv (colon)': [':', 'simple'],
+	'csv (double quote)': ['"', 'simple'],
+	'csv (equals)': ['=', 'simple'],
+	'csv (dot)': ['.', 'simple'],
+	'csv (whitespace)': [' ', 'whitespace'],
+	'csv (hyphen)': ['-', 'simple']
+};
+
+function mapSeperatorToLanguageID(separator: string) {
+	for (let languageID in DIALECT_MAP) {
+		if (!DIALECT_MAP.hasOwnProperty(languageID)) {
+			continue;
+		}
+		if (DIALECT_MAP[languageID][0] === separator) {
+			return languageID;
+		}
+	}
+	return null;
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	var fs = require('fs');
+	var occurances = new Map<string, [vscode.Uri, number][]>();
 
 	let disposable = vscode.commands.registerCommand('csv-translation-helper.addMissingTranslatableStrings', () => {
 		let progOptions: vscode.ProgressOptions = { location: vscode.ProgressLocation.Notification, cancellable: true, title: "Scanning for translatable strings..." };
@@ -17,20 +49,25 @@ export function activate(context: vscode.ExtensionContext) {
 				if (vscode.window.activeTextEditor?.document.uri.fsPath === file.fsPath) {
 					continue;
 				}
-				let fileContent = fs.readFileSync(file.fsPath, 'utf8');
-				let translatableStringRegex = /"(![A-Z0-9_]*)"/g;
-				let translatableStringMatches = fileContent.matchAll(translatableStringRegex);
+				let fileContent: string = fs.readFileSync(file.fsPath, 'utf8');
+				let translatableStringMatches = fileContent.matchAll(TRANSLATABLE_REGEX);
 				if (translatableStringMatches) {
 					for (let match of translatableStringMatches) {
 						translatableStrings.add(match[1]);
+						if (match.index) {
+							if (occurances.has(match[1])) {
+								occurances.get(match[1])?.push([file, match.index]);
+							} else {
+								occurances.set(match[1], [[file, match.index]]);
+							}
+						}
 					}
 				}
 			}
 
 			if (vscode.window.activeTextEditor) {
 				let activeFileContent = fs.readFileSync(vscode.window.activeTextEditor.document.uri.fsPath, 'utf8');
-				let translatedStringRegex = /(![A-Z0-9_]*)/g;
-				let translatedStringMatches = activeFileContent.match(translatedStringRegex);
+				let translatedStringMatches = activeFileContent.match(TRANSLATED_REGEX);
 				if (translatedStringMatches) {
 					for (let match of translatedStringMatches) {
 						if (translatableStrings.has(match)) {
@@ -56,9 +93,57 @@ export function activate(context: vscode.ExtensionContext) {
 		});
 
 		context.subscriptions.push(disposable);
-
-		console.log('CSV Translation Helper is activated');
 	});
+
+	let makeHoverInfo = async function (document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken):
+		Promise<vscode.Hover | null | undefined> {
+		let hoverText: (vscode.MarkdownString | string)[] = [];
+
+		let lnum = position.line;
+		let line = document.lineAt(lnum).text;
+		let match = line.match(TRANSLATED_REGEX);
+		if (match) {
+			let translatedString = match[0];
+			if (typeof (translatedString) === 'string') {
+				var translatedStringOccurances = occurances.get(translatedString);
+				if (translatedStringOccurances) {
+					for (let translatedStringOccurance of translatedStringOccurances) {
+						let fileURI = translatedStringOccurance[0];
+						let fileIndex = translatedStringOccurance[1];
+						let matchDocument = await vscode.workspace.openTextDocument(fileURI);
+						let matchPosition = matchDocument.positionAt(fileIndex);
+						let matchLine = matchDocument.lineAt(matchPosition.line);
+						let fileName = fileURI.path.substr(fileURI.path.lastIndexOf('/') + 1);
+						let hoverLine = new vscode.MarkdownString();
+						hoverLine.appendText('['.concat(fileName).concat(`]: `));
+						hoverLine.appendCodeblock(matchLine.text);
+						hoverText.push(hoverLine);
+					}
+				}
+			}
+		}
+
+		if (hoverText.length === 0) {
+			hoverText.push('No matches found!');
+		}
+
+		return new Promise(resolve => {
+			resolve({
+				contents: hoverText
+			});
+		});
+	};
+
+	for (let csvDialect in DIALECT_MAP) {
+		if (DIALECT_MAP.hasOwnProperty(csvDialect)) {
+			let hoverProvider = vscode.languages.registerHoverProvider(csvDialect, {
+				provideHover(document, position, token) {
+					return makeHoverInfo(document, position, token);
+				}
+			});
+			context.subscriptions.push(hoverProvider);
+		}
+	}
 }
 
 // this method is called when your extension is deactivated
